603c94f04370279b1d59239fbedcbeec3b4d7005
swarley-maan <xavier98m@gmail.com>
2020-04-03 18:19:54 +0100

    layout+

--- app/src/main/java/pt/ipg/varioscampos/MainActivity.java
+++ app/src/main/java/pt/ipg/varioscampos/MainActivity.java
@@ -3,6 +3,7 @@ package pt.ipg.varioscampos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void sendMessage(View view) {
+        // Do something in response to button
     }
 }
